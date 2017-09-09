855313fbfe193c5ebc82f2876ed7accae4b29bc6
juliedwhite <jdw345@psu.edu>
2017-09-09 12:45:19 -0400

    Remove SNPs with MAF < 0.05 and added chr X to 1000G merge

--- ProcessingScript.py
+++ ProcessingScript.py
@@ -7,8 +7,10 @@
 # -Update maternal and paternal IDs
 # -Update sex
 # -Using 1000 Genomes as a reference (this part based off Perl script by W. Rayner, 2015, wrayner@well.ox.ac.uk
+#   -Removes SNPs with MAF < 5% in study dataset
 #   -Removes SNPs not in 1000 Genomes
 #   -Removes all A/T G/C SNPs with MAF > 40% in the reference data set
+#   -Removes snps with MAF < 5% in study dataset
 #   -Removes all SNPs with an AF difference >0.2, between reference and dataset frequency file, frequency file is
 #       expected to be a plink frequency file with the same number of SNPs as the bim file
 #   -Removes duplicates that may be introduced with the position update
@@ -29,7 +31,7 @@ to_do = input('What would you like to do?\n'
               'New FID, New IID.\n'
               '3) Update parental IDs. You need a file with FID, IID, Paternal IID, and Maternal IID.\n'
               '4) Update sex. You need a file with FID, IID, Sex (M = 1, F = 2, Unknown = 0)\n'
-              '5) Remove SNPs that are not in 1000 Genomes Phase 3\n'
+              '5) Merge with 1000 Genomes Phase 3\n'
               '6) Nothing\n'
               'Please enter a number (i.e. 2): ')
 
@@ -101,45 +103,71 @@ elif to_do == '4':
               + '_SexUpdated')
 
 elif to_do == '5':
-    # Remove SNPs not in 1000 Genomes.
-    # Match the position in 1000 genomes with the position in our genotype file, on a chromosome by chromosome basis.
+    # Remove SNPs with MAF < 0.05, because they are often not imputed well
+    geno_name = input('Please enter the name of the genotype files (without bed/bim/fam extension: ')
+    os.system('plink --bfile ' + geno_name + ' -maf 0.05 --make-bed --out ' + geno_name + '_MAF0.05')
 
+    # Remove SNPs that are not in 1000 G Phase 3
+    # This is where I got the reference files: https://mathgen.stats.ox.ac.uk/impute/1000GP_Phase3.html
     import pandas as pd
 
-    geno_name = input('Please enter the name of the genotype files (without bed/bim/fam extension: ')
-    bim_file = pd.read_csv(geno_name + '.bim', sep="\t", header=None)
+    bim_file = pd.read_csv(geno_name + '_MAF0.05.bim', sep="\t", header=None)
     # Columns of bim_file: chr, rsid, cm, bp, allele1, allele2
     # Columns of legend: id, position, a0, a1, type, AFR, AMR, EAS, EUR, SAS, ALL
 
-    file_save_names = ['chr%d_position_file' % x for x in range(1, 23)]
+    snps_by_chr = ['chr%d_snps' % x for x in range(1, 23)]
     legend_file_names = ['1000GP_Phase3_chr%d.legend' % x for x in range(1, 23)]
 
-# Write lists of SNPs that are in 1000 Genomes
-    for i in range(0, len(file_save_names)):
+    # Match the position in 1000 genomes with the position in our genotype file, on a chromosome by chromosome basis.
+    for i in range(0, len(snps_by_chr)):
         current_legend_file = pd.read_csv(legend_file_names[i], sep=" ", header=0)
         print('Successfully read in chr' + str(i + 1) + ' legend file')
-        file_save_names[i] = pd.merge(left=bim_file.loc[bim_file[0] == i + 1], right=current_legend_file, how='inner',
+        snps_by_chr[i] = pd.merge(left=bim_file.loc[bim_file[0] == i + 1], right=current_legend_file, how='inner',
                                       left_on=3, right_on='position')
         print('chr' + str(i + 1) + ' complete')
 
-# Create new plink files only with these SNPs.
-    all_chr_files = [file_save_names[0], file_save_names[1], file_save_names[2], file_save_names[3], file_save_names[4],
-                 file_save_names[5], file_save_names[6], file_save_names[7], file_save_names[8], file_save_names[9],
-                 file_save_names[10], file_save_names[11], file_save_names[12], file_save_names[13], file_save_names[14],
-                 file_save_names[15], file_save_names[16], file_save_names[17], file_save_names[18], file_save_names[19],
-                 file_save_names[20], file_save_names[21]]
+    # Chromosome X
+    # Plink codes the pseudoautosomal region as 23, for now I won't include the non-pseudoautosomal regions since they
+    #   need to be treated differently.
+    current_legend_file = pd.read_csv('1000GP_Phase3_chrX_PAR1.legend', sep = " ", header = 0)
+    print('Successfully read in chrX pseudoautosomal region 1 legend file')
+    chrx_PAR1 = pd.merge(left=bim_file.loc[bim_file[0] == 23], right = current_legend_file, how='inner', left_on=3,
+                           right_on='position')
+    print('chrX pseudoautosomal region 1 done')
+
+    current_legend_file = pd.read_csv('1000GP_Phase3_chrX_PAR2.legend', sep=" ", header=0)
+    print('Successfully read in chrX pseudoautosomal region 2 legend file')
+    chrx_PAR2 = pd.merge(left=bim_file.loc[bim_file[0] == 23], right=current_legend_file, how='inner', left_on=3,
+                         right_on='position')
+    print('chrX pseudoautosomal region 2 done')
+
+    # Removes all A/T G/C SNPs with MAF > 40% in the reference data set
+    # Use the all_chr_files to identify which snps we're already keeping have MAF > 40% in the reference 'ALL' column.
+    #if snps_by_chr[0].loc[snps_by_chr[0]['ALL'] <= 0.5]:
+     #   temp_alt_maf = snps_by_chr[0].loc[snps_by_chr[0]['ALL'] <= 0.4]
+    #elif snps_by_chr[0].loc[snps_by_chr[0]['ALL'] > 0.5]:
+    #    temp_ref_maf = snps_by_chr[0].loc[1 - snps_by_chr[0]['ALL'] <= 0.4]
+    #else:
+    #    print("Something is wrong")
+
+    # Remove SNPs not in 1000 Genomes and create new plink file
+    all_chr_files = [snps_by_chr[0], snps_by_chr[1], snps_by_chr[2], snps_by_chr[3], snps_by_chr[4],
+                 snps_by_chr[5], snps_by_chr[6], snps_by_chr[7], snps_by_chr[8], snps_by_chr[9],
+                 snps_by_chr[10], snps_by_chr[11], snps_by_chr[12], snps_by_chr[13], snps_by_chr[14],
+                 snps_by_chr[15], snps_by_chr[16], snps_by_chr[17], snps_by_chr[18], snps_by_chr[19],
+                 snps_by_chr[20], snps_by_chr[21], chrx_PAR1, chrx_PAR2]
     all_snps_in_1000g = pd.concat(all_chr_files)
     print(all_snps_in_1000g.head(3))
     unique_snps_in_1000g = all_snps_in_1000g.drop_duplicates(subset = 1, keep = 'first', inplace = False)
     unique_snps_in_1000g[1].to_csv('SNPs_in_1000G.txt', sep = '\t', header = False, index = False)
-    os.system('plink --bfile ' + geno_name + ' --extract SNPs_in_1000G.txt --make-bed --out ' + geno_name
-              + '_SNPsIn1000G')
+    os.system('plink --bfile ' + geno_name + '_MAF0.05 --extract SNPs_in_1000G.txt --make-bed --out ' + geno_name
+              + '_MAF0.05_SNPsIn1000G')
 
 elif to_do == '6':
     print("You go, couch potato")
 
 else:
-    print("Please enter a number 1-5.")
+    print("Please enter a number 1-6.")
 
 
 
