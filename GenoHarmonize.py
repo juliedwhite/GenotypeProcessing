dca5d366bb5c8006b9b834a048f4ec9078c71ba8
juliedwhite <jdw345@psu.edu>
2017-12-14 17:09:06 -0500

    Merge harmonization snp updates into one file. Updates to file removal langauge. Find snpflip source code. Don't remove ambiguous snps.

--- GenoHarmonize.py
+++ GenoHarmonize.py
@@ -19,10 +19,12 @@ def harmonize_with_1000g(geno_name):
     import sys
     import pandas as pd
     import numpy as np
+    import csv
+    import gzip
 
     # Get current working directory.
     orig_wd = os.getcwd()
-
+    
     # Getting required reference files and genotype harmonizer program
     # Ask the user if they already have the 1000G Phase 3 vcf files.
     vcf_exists = input('\u001b[34;1m Have you already downloaded the 1000G Phase3 VCF files? (y/n): \u001b[0m').lower()
@@ -60,7 +62,7 @@ def harmonize_with_1000g(geno_name):
         legend_path = os.path.join(os.getcwd(), '1000G_Phase3_HapLegendSample')
     else:
         sys.exit('Please answer yes or no. Quitting now because no legend files.')
-
+    
     # Ask if they have the hg19 fasta files.
     fasta_exists = input('\u001b[32;1m Have you already downloaded the 1000G hg19 fasta file? (y/n): \u001b[0m').lower()
 
@@ -78,7 +80,7 @@ def harmonize_with_1000g(geno_name):
         fasta_path = os.path.join(os.getcwd(), '1000G_hg19_fasta')
     else:
         sys.exit('Please answer yes or no. Quitting now because no fasta file.')
-
+    
     # Ask if they have genotype harmonizer.
     harmonizer_exists = input('\u001b[34;1m Have you already downloaded Genotype Harmonizer? (y/n): \u001b[0m').lower()
 
@@ -118,10 +120,10 @@ def harmonize_with_1000g(geno_name):
         for file in plink_files:
             print(file)
             shutil.copy(file, 'Harmonized_To_1000G')
-
+    
     # Switch to this directory.
     os.chdir('Harmonized_To_1000G')
-
+    
     # Make the lists that we're going to need, since this is on a per chromosome basis.
     vcf_file_names = ['ALL.chr%d.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz' % x for x in
                       range(1, 23)]
@@ -129,12 +131,17 @@ def harmonize_with_1000g(geno_name):
     legend_file_names = ['1000GP_Phase3_chr%d.legend.gz' % x for x in range(1, 23)]
     legend_file_names.extend(['1000GP_Phase3_chrX_NONPAR.legend.gz'])
     harmonized_geno_names = [geno_name + '_chr%d_Harmonized' % x for x in range(1, 24)]
+    id_updates = [s + '_idUpdates.txt' for s in harmonized_geno_names]
+    id_update_names = [s + '_idUpdates.txt' for s in harmonized_geno_names]
+    snp_logs = [s + '_snpLog.log' for s in harmonized_geno_names]
+    snp_log_names = [s + '_snpLog.log' for s in harmonized_geno_names]
     freq_file_names = [geno_name + '_chr%d_Harmonized.frq' % x for x in range(1, 24)]
     af_diff_removed_by_chr = ['chr%d_SNPsRemoved_AFDiff' % x for x in range(1, 24)]
     final_snps_by_chr = ['chr%d_SNPsKept' % x for x in range(1, 24)]
     final_snp_lists = ['chr%d_SNPsKept.txt' % x for x in range(1, 24)]
     af_checked_names = [geno_name + '_chr%d_HarmonizedTo1000G' % x for x in range(1, 24)]
 
+    
     # Harmonize for each chromosome
     for i in range(0, len(vcf_file_names)):
         # Call genotype harmonizer for autosomes
@@ -152,6 +159,12 @@ def harmonize_with_1000g(geno_name):
                                     '--update-reference-allele '
                                     '--outputType PLINK_BED '
                                     '--output ' + harmonized_geno_names[i])
+            
+            id_updates[i] = pd.read_csv(id_update_names[i], sep='\t', header=0,
+                                        dtype={'chr':str, 'pos':int, 'originalId':str, 'newId':str})
+            snp_logs[i] = pd.read_table(snp_log_names[i], sep='\t', header=0,
+                                      dtype={'chr': str, 'pos': int, 'id': str, 'alleles': str, 'action':str,
+                                             'message':str})
 
         elif i == 22:
             # Since chr X is labeled as 23 in the plink files and X in the vcf files, we need to separate it out and
@@ -170,8 +183,41 @@ def harmonize_with_1000g(geno_name):
                                     + '" --refType VCF --hweFilter 0.01 --mafFilter 0.05 --update-id --debug '
                                       '--mafAlign 0 --update-reference-allele --outputType PLINK_BED --output '
                                     + harmonized_geno_names[i])
+            subprocess.call('rm ' + geno_name + '_chr23.*')
+            
+            id_updates[i] = pd.read_csv(id_update_names[i], sep='\t', header=0,
+                                        dtype={'chr': str, 'pos': int, 'originalId': str, 'newId': str})
+            snp_logs[i] = pd.read_csv(snp_log_names[i], sep='\t', header=0,
+                                   dtype={'chr': str, 'pos': int, 'id': str, 'alleles': str, 'action': str,
+                                          'message': str})
         else:
             sys.exit("\u001b[36;1m Something is wrong with the number/name of reference files \u001b[0m")
+
+    # Concatenate all of the id updates into one file.
+    all_id_updates = pd.concat([id_updates[0], id_updates[1], id_updates[2], id_updates[3], id_updates[4],
+                                id_updates[5], id_updates[6], id_updates[7], id_updates[8], id_updates[9],
+                                id_updates[10], id_updates[11], id_updates[12], id_updates[13], id_updates[14],
+                                id_updates[15], id_updates[16], id_updates[17], id_updates[18], id_updates[19],
+                                id_updates[20], id_updates[21], id_updates[22]])
+    # Write list to text file.
+    all_id_updates.to_csv('Harmonization_ID_Updates.txt', sep='\t', header=True, index=False)
+
+    # Remove the clutter
+    if os.path.getsize('Harmonization_ID_Updates.txt') > 0:
+        for f in id_update_names:
+            subprocess.call('rm ' + f)
+
+    all_snp_logs = pd.concat([snp_logs[0], snp_logs[1], snp_logs[2], snp_logs[3], snp_logs[4], snp_logs[5], snp_logs[6],
+                              snp_logs[7], snp_logs[8], snp_logs[9], snp_logs[10], snp_logs[11], snp_logs[12],
+                              snp_logs[13], snp_logs[14], snp_logs[15], snp_logs[16], snp_logs[17], snp_logs[18],
+                              snp_logs[19], snp_logs[20], snp_logs[21], snp_logs[22]])
+    # Write list to text file.
+    all_snp_logs.to_csv('Harmonization_SNP_Logs.txt', sep='\t', header=True, index=False)
+
+    # Remove the clutter
+    if os.path.getsize('Harmonization_SNP_Logs.txt') > 0:
+        for f in snp_log_names:
+            subprocess.call('rm ' + f)
 
     # Now for each that was just harmonized, remove all SNPs with an allele (AF) difference > 0.2 since we are going to
     # use a global reference population between study dataset and all superpopulation allele frequencies. IF within 0.2
@@ -313,19 +359,13 @@ def harmonize_with_1000g(geno_name):
         # Remove extra files that we don't need anymore. These were files that were harmonized, but not checked for
         # allele frequency differences.
         if os.path.getsize(af_checked_names[i] + '.bim') > 0:
-            subprocess.call('rm ' + str(final_snps_by_chr[i]) + '.txt')
-            subprocess.call('rm ' + str(harmonized_geno_names[i]) + '.bed')
-            subprocess.call('rm ' + str(harmonized_geno_names[i]) + '.bim')
-            subprocess.call('rm ' + str(harmonized_geno_names[i]) + '.fam')
-            subprocess.call('rm ' + str(harmonized_geno_names[i]) + '.log')
-            subprocess.call('rm ' + str(harmonized_geno_names[i]) + '.frq')
-            subprocess.call('rm ' + str(harmonized_geno_names[i]) + '.nosex')
-            subprocess.call('rm ' + str(harmonized_geno_names[i]) + '.hh')
-
+            subprocess.call('rm ' + final_snp_lists[i])
+            subprocess.call('rm ' + harmonized_geno_names[i] + '.*')
+            
         # Done with one chromosome.
 
         print('\u001b[36;1m Finished with chr' + str(i + 1) + '\u001b[0m')
-
+    
     # Make a big list of all SNPs removed and all SNPs kept just for reference purposes.
     all_snps_removed = pd.concat([af_diff_removed_by_chr[0], af_diff_removed_by_chr[1], af_diff_removed_by_chr[2],
                                   af_diff_removed_by_chr[3], af_diff_removed_by_chr[4], af_diff_removed_by_chr[5],
@@ -336,7 +376,7 @@ def harmonize_with_1000g(geno_name):
                                   af_diff_removed_by_chr[18], af_diff_removed_by_chr[19], af_diff_removed_by_chr[20],
                                   af_diff_removed_by_chr[21], af_diff_removed_by_chr[22]])
     # Write list to text file.
-    all_snps_removed.to_csv('SNPs_Removed_AFDiff.txt', sep='\t', header=True, index=False)
+    all_snps_removed.to_csv('SNPs_Removed_AFCheck.txt', sep='\t', header=True, index=False)
 
     # Make one big list of all SNPs kept
     all_snps_kept = pd.concat([final_snps_by_chr[0], final_snps_by_chr[1], final_snps_by_chr[2],
@@ -348,8 +388,8 @@ def harmonize_with_1000g(geno_name):
                                final_snps_by_chr[18], final_snps_by_chr[19], final_snps_by_chr[20],
                                final_snps_by_chr[21], final_snps_by_chr[22]])
     # Write this list to a text file.
-    all_snps_kept.to_csv('SNPs_Kept.txt', sep='\t', header=True, index=False)
-
+    all_snps_kept.to_csv('SNPs_Kept_AFCheck.txt', sep='\t', header=True, index=False)
+    
     # Merge harmonized dataset genotypes
     with open("HouseMergeList.txt", "w") as f:
         wr = csv.writer(f, delimiter="\n")
@@ -359,63 +399,73 @@ def harmonize_with_1000g(geno_name):
 
     if os.path.getsize(geno_name + '_HarmonizedTo1000G.bim') > 0:
         for i in range(0, len(af_checked_names)):
-            subprocess.call('rm ' + str(af_checked_names[i]) + '.bed')
-            subprocess.call('rm ' + str(af_checked_names[i]) + '.bim')
-            subprocess.call('rm ' + str(af_checked_names[i]) + '.fam')
-            subprocess.call('rm ' + str(af_checked_names[i]) + '.log')
-            subprocess.call('rm ' + str(af_checked_names[i]) + '.nosex')
-            subprocess.call('rm ' + str(af_checked_names[i])+ '.hh')
+            subprocess.call('rm ' + str(af_checked_names[i]) + '.*')
+
     else:
         sys.exit("\u001b[36;1m For some reason the house gentoypes did not merge. You should try it manually \u001b[0m")
-
+    
     #Check to make sure the snps are on the same strand as the reference
     # First need to change the chromosome names to match the fasta file so they can match.
     # Read chrX file into pandas
-    bim_file = pd.read_csv(geno_name + '_HarmonizedTo1000G.bim', sep='\t', header=None)
+    bim_file = pd.read_csv(geno_name + '_HarmonizedTo1000G.bim', sep='\t', header=None,
+                           dtype={0:str, 1:str, 2:int, 3:int, 4: str, 5:str})
     # Replace '23' with 'X', which is how the fasta file calls X
-    bim_file.iloc[:, 0].replace(23, 'X', inplace=True)
+    bim_file.iloc[:, 0].replace('23', 'X', inplace=True)
     # Replace '24' with 'Y', which is how the fasta file calls Y
-    bim_file.iloc[:, 0].replace(24, 'Y', inplace=True)
+    bim_file.iloc[:, 0].replace('24', 'Y', inplace=True)
     # Replace '26' with 'MT' which is how the fasta file calls the mitochondrial DNA
-    bim_file.iloc[:, 0].replace(26, 'MT', inplace=True)
+    bim_file.iloc[:, 0].replace('26', 'MT', inplace=True)
     # Write new genotype
     bim_file.to_csv(geno_name + '_HarmonizedTo1000G.bim', sep='\t', header=False, index=False, na_rep='NA')
 
+    # Fasta file needs to be unzipped for snpflip to work.
+    if os.path.exists(os.path.join(fasta_path, 'human_g1k_v37.fasta')):
+        pass
+    elif os.path.exists(os.path.join(fasta_path, 'human_g1k_v37.fasta.gz')):
+        with gzip.open(os.path.join(fasta_path, 'human_g1k_v37.fasta.gz'), 'rb') as f_in, \
+                open(os.path.join(fasta_path, 'human_g1k_v37.fasta'), 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+    else:
+        sys.exit("Quitting because I cannot find the fasta file.")
+
     try:
-        subprocess.check_output('snpflip --fasta-genome = ' + os.path.join(fasta_path, 'human_g1k_v37.fasta.gz')
-                                + ' --bim-file = ' + geno_name + '_HarmonizedTo1000G --output-prefix = ' + geno_name
+        # Find where snpflip is.
+        for r, d, f in os.walk("C:\\"):
+            for files in f:
+                if files == "snpflip":
+                    snpflip_path = (os.path.join(r, files))
+        # Perform flip check.
+        subprocess.check_output('python ' + snpflip_path + ' --fasta-genome "'
+                                + os.path.join(fasta_path, 'human_g1k_v37.fasta')
+                                + '" --bim-file ' + geno_name + '_HarmonizedTo1000G.bim --output-prefix ' + geno_name
                                 + '_HarmonizedTo1000G')
-    except ImportError:
+
+    except:
         # Import module where I have snpflip
         import genodownload
         # Download snpflip
         genodownload.snpflip()
-        # Re-do snpflip process.
-        subprocess.check_output('snpflip --fasta-genome = ' + os.path.join(fasta_path, 'human_g1k_v37.fasta.gz')
-                                + ' --bim-file = ' + geno_name + '_HarmonizedTo1000G --output-prefix = ' + geno_name
-                                + '_HarmonizedTo1000G')
+        # Find where snpflip is:
+        for r, d, f in os.walk("C:\\"):
+            for files in f:
+                if files == "snpflip":
+                    snpflip_path = (os.path.join(r, files))
+        # Re do
+        subprocess.check_output('python ' + snpflip_path + ' --fasta-genome "'
+                                + os.path.join(fasta_path, 'human_g1k_v37.fasta') + '" --bim-file ' + geno_name
+                                + '_HarmonizedTo1000G.bim --output-prefix ' + geno_name + '_HarmonizedTo1000G')
 
-    # If SNPs exist that are on the reverse strand, then flip them. If SNPs exist that are ambiguous, then remove them.
-    if os.path.exists(geno_name + '_HarmonizedTo1000G.reverse') \
-            & os.path.exists(geno_name + '_HarmonizedTo1000G.ambiguous'):
-        subprocess.check_output('plink --bfile ' + geno_name + '_HarmonizedTo1000G --flip '
-                                + geno_name + '_HarmonizedTo1000G.reverse --exclude '
-                                + geno_name + '_HarmonizedTo1000G.ambiguous --make-bed --out '
-                                + geno_name + '_HarmonizedTo1000G_StrandChecked')
-    # If SNPs exist that are on the reverse strand, flip them.
-    elif os.path.exists(geno_name + '_HarmonizedTo1000G.reverse'):
-        subprocess.check_output('plink --bfile ' + geno_name + '_HarmonizedTo1000G --flip '
-                                + geno_name + '_HarmonizedTo1000G.reverse --make-bed --out '
-                                + geno_name + '_HarmonizedTo1000G_StrandChecked')
-    # If SNPs exist that have ambiguous strand, then remove them
-    elif os.path.exists(geno_name + '_HarmonizedTo1000G.ambiguous'):
-        subprocess.check_output('plink --bfile ' + geno_name + '_HarmonizedTo1000G --exclude '
-                                + geno_name + '_HarmonizedTo1000G.ambiguous --make-bed --out '
-                                + geno_name + '_HarmonizedTo1000G_StrandChecked')
-    # If neither of these exist, still make a new file to signify that you've done the strand check.
+    # If SNPs exist that are on the reverse strand, then flip them.
+    # Currently ignores snps that are ambiguous, since I already removed those that would be hard to phase. Could change
+    # this later.
+    if os.path.getsize(geno_name + '_HarmonizedTo1000G.reverse') > 0:
+        subprocess.check_output('plink --bfile ' + geno_name + '_HarmonizedTo1000G --flip ' + geno_name
+                                + '_HarmonizedTo1000G.reverse --make-bed --out ' + geno_name
+                                + '_HarmonizedTo1000G_StrandChecked')
+    # If .reverve doesn't exist, still make a new file to signify that you've done the strand check.
     else:
-        subprocess.check_output('plink --bfile ' + geno_name + '_HarmonizedTo1000G --make-bed --out '
-                                + geno_name + '_HarmonizedTo1000G_StrandChecked')
+        subprocess.check_output('plink --bfile ' + geno_name + '_HarmonizedTo1000G --make-bed --out ' + geno_name
+                                + '_HarmonizedTo1000G_StrandChecked')
 
     # Finished
     shutil.copy2(geno_name + '_HarmonizedTo1000G_StrandChecked.bed', orig_wd)
@@ -424,4 +474,5 @@ def harmonize_with_1000g(geno_name):
 
     # Change back to original working directory.
     os.chdir(orig_wd)
+
 
